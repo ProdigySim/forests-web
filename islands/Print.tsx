ad8b0c3193794d603b892b7aa3719ea48056b6ab
Michael ProdigySim Busby <prodigysim@gmail.com>
2025-11-16 08:43:48 -0600

    Show some special foil types

--- islands/Print.tsx
+++ islands/Print.tsx
@@ -9,7 +9,7 @@ interface PrintProps {
 }
 
 function dateFmt(released_at: Date) {
-  return released_at.toISOString().split('T')[0];
+  return released_at.toISOString().split("T")[0];
 }
 function toCardId(c: Card) {
   const collNumFixed = c.collector_number.replace("★", "");
@@ -19,7 +19,10 @@ function toCardId(c: Card) {
 function shortenSetName(s: string) {
   return s
     .replace("World Championship Decks", "World Champ. Deck")
-    .replace("Fourth Edition Foreign Black Border", "4th Ed. Foreign Black Border")
+    .replace(
+      "Fourth Edition Foreign Black Border",
+      "4th Ed. Foreign Black Border",
+    )
     .replace("The Lord of the Rings", "LotR")
     .replace("Commander Legends:", "CL:")
     .replace("Global Series", "G.S.")
@@ -27,28 +30,52 @@ function shortenSetName(s: string) {
     .replace("Duel Decks Anthology:", "DDA:");
 }
 
+function foilText(card: Card, finish: string) {
+  const promoTypeFoils = card.promo_types?.find((x) =>
+    x.includes("foil")
+  ) as string;
+  switch (promoTypeFoils) {
+    case "surgefoil":
+      return "SURGE ";
+    case "galaxyfoil":
+      return "GALAXY ";
+    case "ripplefoil":
+      return "RIPPLE ";
+    default:
+      return finish === "nonfoil" ? "" : `${finish.toUpperCase()} `;
+  }
+}
+
 export function Print(props: PrintProps) {
   const { index, card, finish } = props;
-  const image = card.image_uris ? card.image_uris.normal : card.card_faces?.[0]?.image_uris?.normal;
-  const finishText = finish === 'nonfoil' ? '' : `${finish.toUpperCase()} `;
+  const image = card.image_uris
+    ? card.image_uris.normal
+    : card.card_faces?.[0]?.image_uris?.normal;
+  const finishText = foilText(card, finish);
 
   const collected = props.collection.value.has(`${card.id}-${finish}`);
-  const toggle = (() => {
+  const toggle = () => {
     const s = new Set(props.collection.value);
-    if(collected) {
+    if (collected) {
       s.delete(`${card.id}-${finish}`);
     } else {
       s.add(`${card.id}-${finish}`);
     }
     props.collection.value = s;
-  });
+  };
   return (
-    <div onClick={toggle} class={'print' + (collected ? ' collected' : '')} id={index.toString(10)} data-scryfall-id={card.id} data-finish={finish}>
-        <div class="num">{index.toString(10).padStart(4, "0")}</div>
-        <img class="front" src={image} />
-        <div class="name">{shortenSetName(card.set_name)}</div>
-        <div class="set">{finishText}{toCardId(card)}</div>
-        <div class="date">{dateFmt(card.released_at)}</div>
-      </div>
+    <div
+      onClick={toggle}
+      class={"print" + (collected ? " collected" : "")}
+      id={index.toString(10)}
+      data-scryfall-id={card.id}
+      data-finish={finish}
+    >
+      <div class="num">{index.toString(10).padStart(4, "0")}</div>
+      <img class="front" src={image} />
+      <div class="name">{shortenSetName(card.set_name)}</div>
+      <div class="set">{finishText}{toCardId(card)}</div>
+      <div class="date">{dateFmt(card.released_at)}</div>
+    </div>
   );
 }
